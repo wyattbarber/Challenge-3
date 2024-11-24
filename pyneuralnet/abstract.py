37e8e2715cb9efa03410903060486431530a614f
Wyatt Barber <wyattjbarber@gmail.com>
2024-11-24 17:51:46 -0500

    Update python compound and abstract models

--- pyneuralnet/abstract.py
+++ pyneuralnet/abstract.py
@@ -1,1 +1,36 @@
-from neuralnet import DataSource, DataSource2D, Model, Model2D, LossBase
+from neuralnet import DataSource, DataSource2D, LossBase
+from neuralnet import Model as _Model
+from neuralnet import Model2D as _Model2D
+from neuralnet import Encoder as _Encoder
+from neuralnet import Encoder2D as _Encoder2D
+
+
+class Model(_Model, _Model2D):
+    """
+    Abstract base for model objects.
+
+    Defines default implementations for common python
+    operations, such as pickling.
+    """
+    def __init__(self):
+        _Model.__init__(self)
+        _Model2D.__init__(self)
+
+    def __getstate__(self):
+        return self.__dict__
+    
+    def __setstate__(self, state):
+        self.__dict__  = state
+
+
+class Encoder(Model, _Encoder, _Encoder2D):
+    """
+    Abstract base for encoder objects.
+
+    Defines default implementations for common python
+    operations, such as pickling.
+    """
+    def __init__(self):
+        Model.__init__(self)
+        _Encoder.__init__(self)
+        _Encoder2D.__init__(self)
--- pyneuralnet/compound.py
+++ pyneuralnet/compound.py
@@ -1,7 +1,7 @@
-from neuralnet import Model, Model2D, Encoder, Encoder2D, LossBase
+from .abstract import Model, Encoder
 from typing import List, Any
 
-class Sequence(Model, Model2D):
+class Sequence(Model):
     _layers : List[Any]
 
     def __init__(self, *layers):
@@ -11,7 +11,6 @@ class Sequence(Model, Model2D):
         :param layers: All layers, from input to output.
         """
         Model.__init__(self)
-        Model2D.__init__(self)
         self._layers = layers
 
     def forward(self, input):
@@ -24,6 +23,53 @@ class Sequence(Model, Model2D):
         h = [error]
         for l in self._layers[::-1]:
             h.append(l.backward(h[-1]))
+        return h[-1]
+    
+    def update(self, rate):
+        for l in self._layers:
+            l.update(rate)
+
+
+class DeepEncoder(Encoder):
+    _layers : List[Any]
+
+    def __init__(self, *layers):
+        """ 
+        Construct an encoder out of nested encoder components.
+
+        :param layers: All layers, from input to latent layer.
+        """
+        Encoder.__init__(self)
+        self._layers = layers
+
+    def forward(self, input):
+        return self.decode(self.encode(input))
+    
+    def backward(self, error):
+        return self.backward_encode(self.backward_decode(error))
+    
+    def encode(self, input):
+        h = [input]
+        for l in self._layers:
+            h.append(l.encode(h[-1]))
+        return h[-1]
+    
+    def decode(self, embed):
+        h = [embed]
+        for l in self._layers[::-1]:
+            h.append(l.decode(h[-1]))
+        return h[-1]
+    
+    def backward_encode(self, error):
+        h = [error]
+        for l in self._layers[::-1]:
+            h.append(l.backward_encode(h[-1]))
+        return h[-1]
+    
+    def backward_decode(self, error):
+        h = [error]
+        for l in self._layers:
+            h.append(l.backward_decode(h[-1]))
         return h[-1]
     
     def update(self, rate):
